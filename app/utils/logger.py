cfd27d3187e419ad663a1a52a8424644840b4785
HOME-PC\Erik <PayDay892@mail.ru>
2026-07-24 07:13:22 +0600

    Автоматическое создание папки Logs

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -4,23 +4,32 @@ from pathlib import Path
 from app.config import settings
 
 
-log_dir = Path("logs")
-log_dir.mkdir(exist_ok=True)
+log_path = Path(settings.LOG_FILE)
+
+log_path.parent.mkdir(
+    parents=True,
+    exist_ok=True
+)
+
 
 logger = logging.getLogger("backend")
 
 logger.setLevel(logging.INFO)
 
-handler = logging.FileHandler(
-    settings.LOG_FILE,
+
+file_handler = logging.FileHandler(
+    log_path,
     encoding="utf-8"
 )
+
 
 formatter = logging.Formatter(
     "%(asctime)s | %(levelname)s | %(message)s"
 )
 
-handler.setFormatter(formatter)
+
+file_handler.setFormatter(formatter)
+
 
 logger.handlers.clear()
-logger.addHandler(handler)
+logger.addHandler(file_handler)
