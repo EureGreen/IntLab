c0ebd5d363b6bf950abc186d23322d5223ecf8bc
HOME-PC\Erik <PayDay892@mail.ru>
2026-07-24 06:59:14 +0600

    Обновлены логи + деплой

--- app/middleware/logger.py
+++ app/middleware/logger.py
@@ -7,41 +7,31 @@ from app.utils.logger import logger
 
 class LoggingMiddleware(BaseHTTPMiddleware):
 
-    async def dispatch(
-        self,
-        request,
-        call_next
-    ):
+    async def dispatch(self, request, call_next):
 
-        start_time = time.time()
+        start_time = time.perf_counter()
 
         try:
-
             response = await call_next(request)
 
         except Exception:
-
             logger.exception(
-                "Unhandled request error"
+                "Unhandled exception | %s %s | ip=%s",
+                request.method,
+                request.url.path,
+                request.client.host,
             )
-
             raise
 
-
-        process_time = round(
-            (time.time() - start_time) * 1000,
-            2
-        )
-
+        process_time = (time.perf_counter() - start_time) * 1000
 
         logger.info(
-            "%s %s | status=%s | time=%sms | ip=%s",
+            "ip=%s | %s %s | status=%d | %.2f ms",
+            request.client.host,
             request.method,
             request.url.path,
             response.status_code,
             process_time,
-            request.client.host
         )
-
 
         return response
--- app/utils/logger.py
+++ app/utils/logger.py
@@ -4,29 +4,23 @@ from pathlib import Path
 from app.config import settings
 
 
-Path("data/logs").mkdir(
-    parents=True,
-    exist_ok=True
-)
-
+log_dir = Path("logs")
+log_dir.mkdir(exist_ok=True)
 
 logger = logging.getLogger("backend")
 
 logger.setLevel(logging.INFO)
 
+handler = logging.FileHandler(
+    settings.LOG_FILE,
+    encoding="utf-8"
+)
 
 formatter = logging.Formatter(
     "%(asctime)s | %(levelname)s | %(message)s"
 )
 
+handler.setFormatter(formatter)
 
-file_handler = logging.FileHandler(
-    settings.LOG_FILE,
-    encoding="utf-8"
-)
-
-file_handler.setFormatter(formatter)
-
-
-if not logger.handlers:
-    logger.addHandler(file_handler)
+logger.handlers.clear()
+logger.addHandler(handler)
